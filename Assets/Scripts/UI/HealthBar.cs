90b88af2f728ebeb5f8962e6065afab6ce1cd21e
PoyoMain <69494961+PoyoMain@users.noreply.github.com>
2025-12-20 09:53:32 -0500

    Made health bar canvas

---
+++ Assets/Scripts/UI/HealthBar.cs
@@ -0,0 +1,39 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    [Header("Listen Events")]
+    [SerializeField] private IntEventSO healthSetEventSO;
+    [SerializeField] private IntEventSO healthUpdatedEventSO;
+
+    private Slider slider;
+
+    private void Awake()
+    {
+        slider = GetComponentInChildren<Slider>();
+    }
+
+    private void OnEnable()
+    {
+        healthSetEventSO.onEventRaised += InitializeBar;
+        healthUpdatedEventSO.onEventRaised += UpdateHealth;
+    }
+
+    private void OnDisable()
+    {
+        healthSetEventSO.onEventRaised -= InitializeBar;
+        healthUpdatedEventSO.onEventRaised -= UpdateHealth;
+    }
+
+    private void InitializeBar(int value)
+    {
+        slider.value = slider.maxValue = value;
+    }
+
+    private void UpdateHealth(int value)
+    {
+        slider.value = value;
+    }
+}
