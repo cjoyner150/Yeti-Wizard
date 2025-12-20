21aaa043c0cbd1cae1a7660c382cafee8d2ee0b0
PoyoMain <69494961+PoyoMain@users.noreply.github.com>
2025-12-19 20:48:36 -0500

    Adding bullet prefab

---
+++ Assets/Scripts/Projectiles/Bullet.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody), typeof(DamageComponent))]
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] private float disappearTime;
+
+    private float speed;
+    private float disappearTimer;
+
+    private Rigidbody rb;
+    private DamageComponent damageComponent;
+
+    private void Awake()
+    {
+        TryGetComponent(out rb);
+        TryGetComponent(out damageComponent);
+    }
+
+    private void Start()
+    {
+        disappearTimer = disappearTime;
+    }
+
+    private void Update()
+    {
+        if (disappearTimer > 0)
+        {
+            disappearTimer -= Time.deltaTime;
+            return;
+        }
+
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+
+    public void Init(int damage, float launchSpeed)
+    {
+        speed = launchSpeed;
+        damageComponent.SetDamage(damage);
+    }
+
+    public void Launch()
+    {
+        rb.AddForce(transform.forward * speed, ForceMode.Impulse);
+    }
+}
